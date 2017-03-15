b9f5d8e63263a4483808d299332d125890bfbdd8
Mattias Cibien <mattias@mattiascibien.net>
2017-03-15 14:29:10 +0000

    Slightly better android project

---
+++ src/sdl2app-android/gen/R.java.d
@@ -0,0 +1,12 @@
+C:\src\sdl2-cross-platform-setup\src\sdl2app-android\gen\org\libsdl\app\R.java \
+ : C:\src\sdl2-cross-platform-setup\src\sdl2app-android\res\drawable-hdpi\ic_launcher.png \
+C:\src\sdl2-cross-platform-setup\src\sdl2app-android\res\drawable-mdpi\ic_launcher.png \
+C:\src\sdl2-cross-platform-setup\src\sdl2app-android\res\drawable-xhdpi\ic_launcher.png \
+C:\src\sdl2-cross-platform-setup\src\sdl2app-android\res\drawable-xxhdpi\ic_launcher.png \
+C:\src\sdl2-cross-platform-setup\src\sdl2app-android\res\layout\main.xml \
+C:\src\sdl2-cross-platform-setup\src\sdl2app-android\res\values\strings.xml \
+C:\src\sdl2-cross-platform-setup\src\sdl2app-android\bin\res\drawable-hdpi\ic_launcher.png \
+C:\src\sdl2-cross-platform-setup\src\sdl2app-android\bin\res\drawable-mdpi\ic_launcher.png \
+C:\src\sdl2-cross-platform-setup\src\sdl2app-android\bin\res\drawable-xhdpi\ic_launcher.png \
+C:\src\sdl2-cross-platform-setup\src\sdl2app-android\bin\res\drawable-xxhdpi\ic_launcher.png \
+C:\src\sdl2-cross-platform-setup\src\sdl2app-android\bin\AndroidManifest.xml \
